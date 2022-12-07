38f934301f2f1c83cb7af7d9f864f00e736a1af8
Tulio Parreiras <tulio_parreiras@hotmail.com>
2022-12-07 14:57:15 +1100

    Section 3: Handling User Input

---
+++ Landmarks/Landmarks/UI/Landmarks/LandmarkList.swift
@@ -0,0 +1,55 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Tulio de Oliveira Parreiras on 7/12/2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        return modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
+
+//struct LandmarkList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ForEach(["iPhone SE (3rd generation)", "iPhone 8 Plus", "iPad (9th generation)"], id: \.self) { deviceName in
+//            LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
+//    }
+//}
