d26de0b969dddf92926f3b285f2e2377a018cebb
Tulio Parreiras <tulio_parreiras@hotmail.com>
2022-12-07 12:30:30 +1100

    Section 1: Creating and Combining Views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tulio de Oliveira Parreiras on 7/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
