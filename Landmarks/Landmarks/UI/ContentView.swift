eee1be4007d62b147fb2a3b7c7a4032b8dea587d
Tulio Parreiras <tulio_parreiras@hotmail.com>
2022-12-07 13:04:37 +1100

    Section 2: Building Lists and Navigation

---
+++ Landmarks/Landmarks/UI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Tulio de Oliveira Parreiras on 7/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
